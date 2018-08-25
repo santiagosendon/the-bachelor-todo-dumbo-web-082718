502ac7aff28121cdde5149290581a7066cdfe4e3
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-25 21:48:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,5 +50,18 @@ data.each do |season, array|
 end
 
 def get_average_age_for_season(data, season)
+  age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+   answer = (age/count).round
+return answer
+
   
 end
