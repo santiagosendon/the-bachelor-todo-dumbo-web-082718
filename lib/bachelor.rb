38c67aa7fc0bf64c96cbdb656c7f789fc568ad77
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-25 21:43:41 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # code here   data[season].each do |people|
+       people.each do |k,v|
+        if v == "Winner"
+            full_name = people["name"]
+            return full_name.split(' ').first
+        end 
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
