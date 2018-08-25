61046e3db41def4432dd697793ea0c89d141853f
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-25 21:46:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,8 +8,9 @@ def get_first_name_of_season_winner(data, season)
       end
     end
 end
+
 def get_contestant_name(data, occupation)
-  # code here
+
 end
 
 def count_contestants_by_hometown(data, hometown)
